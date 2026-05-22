be48d7cf7033eb7b590a2aad23fc27302a16f91a
khovan123 <minhpnq1807@gmail.com>
2026-05-22 11:04:16 +0200

    conf: jest env

--- webapp/src/proxy.ts
+++ webapp/src/proxy.ts
@@ -70,6 +70,7 @@ export default async function proxy(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|\\.well-known|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/.well-known/vercel/microfrontends/:path*",
   ],
 }
