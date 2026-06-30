26514973dd2a58906154dd7266fa15c9f10db940
khovan123 <minhpnq1807@gmail.com>
2026-06-30 10:13:07 +0700

    fix: vercel analysis

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react"
 
 import type { Metadata } from "next"
 
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import { Toaster } from "@/components/ui/sonner"
@@ -173,6 +174,7 @@ export default function RootLayout({
             {/* </SocketProvider> */}
           </AppInitializer>
         </ReduxProvider>
+        <Analytics />
       </body>
     </html>
   )
